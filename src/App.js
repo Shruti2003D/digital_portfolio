18d7ac05c0145e5ad39722845b9d2fdd11bdd752
CORP\sd185485 <sd185485@ncr.com>
2025-04-09 14:58:29 +0530

    testing live deployment

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          changed page
         </a>
       </header>
     </div>
